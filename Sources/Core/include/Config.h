b85db0327ea7ca90945fa6f6fc1371b6ec6132c1
Yang Nan <qazyn951230@gmail.com>
2020-09-02 16:49:33 +0800

    New style

--- Sources/Core/include/Config.h
+++ Sources/Core/include/Config.h
@@ -26,22 +26,22 @@
 // ------------- FILE ------------------
 
 #if (__cplusplus)
-#define SP_EXTERN_C_BEGIN   extern "C" {
-#define SP_EXTERN_C_END     }
-#ifdef SP_CPP_USE_NAMESPACE
-#define SP_CPP_FILE_BEGIN   namespace SP_CPP_NAMESPACE { \
-                            _Pragma("clang assume_nonnull begin")
-#define SP_CPP_FILE_END     _Pragma("clang assume_nonnull end") \
-                            }
+    #define SP_EXTERN_C_BEGIN   extern "C" {
+    #define SP_EXTERN_C_END     }
+    #ifdef SP_CPP_USE_NAMESPACE
+        #define SP_CPP_FILE_BEGIN   namespace SP_CPP_NAMESPACE { \
+                                    _Pragma("clang assume_nonnull begin")
+        #define SP_CPP_FILE_END     _Pragma("clang assume_nonnull end") \
+                                    }
+    #else
+        #define SP_CPP_FILE_BEGIN  _Pragma("clang assume_nonnull begin")
+        #define SP_CPP_FILE_END    _Pragma("clang assume_nonnull end")
+    #endif // #ifdef SP_CPP_USE_NAMESPACE
 #else
-#define SP_CPP_FILE_BEGIN  _Pragma("clang assume_nonnull begin")
-#define SP_CPP_FILE_END    _Pragma("clang assume_nonnull end")
-#endif // #ifdef SP_CPP_USE_NAMESPACE
-#else
-#define SP_EXTERN_C_BEGIN
-#define SP_EXTERN_C_END
-#define SP_CPP_FILE_BEGIN   _Pragma("clang assume_nonnull begin")
-#define SP_CPP_FILE_END     _Pragma("clang assume_nonnull end")
+    #define SP_EXTERN_C_BEGIN
+    #define SP_EXTERN_C_END
+    #define SP_CPP_FILE_BEGIN   _Pragma("clang assume_nonnull begin")
+    #define SP_CPP_FILE_END     _Pragma("clang assume_nonnull end")
 #endif // (__cplusplus)
 
 #define SP_C_FILE_BEGIN SP_EXTERN_C_BEGIN \
@@ -52,27 +52,27 @@
 // ------------- FILE ------------------
 
 #if defined(__clang__) || defined(__GNUC__)
-#define SP_LIKELY(x)    __builtin_expect(!!(x), 1)
-#define SP_UNLIKELY(x)  __builtin_expect(!!(x), 0)
+    #define SP_LIKELY(x)    __builtin_expect(!!(x), 1)
+    #define SP_UNLIKELY(x)  __builtin_expect(!!(x), 0)
 #else
-#define SP_LIKELY(x)    (x)
-#define SP_UNLIKELY(x)  (x)
-#endif
+    #define SP_LIKELY(x)    (x)
+    #define SP_UNLIKELY(x)  (x)
+#endif // defined(__clang__) || defined(__GNUC__)
 
 // ------------- NULLABLE ------------------
 // http://clang.llvm.org/docs/AttributeReference.html#nullability-attributes
 // A nullable pointer to non-null pointers to const characters.
 // const char *join_strings(const char * _Nonnull * _Nullable strings, unsigned n);
 #if defined(__clang__)
-// int fetch(int * SP_NONNULL ptr);
-#define SP_NONNULL _Nonnull
-#define SP_NULL_UNSPECIFIED _Null_unspecified
-// int fetch_or_zero(int * SP_NULLABLE ptr);
-#define SP_NULLABLE _Nullable
+    // int fetch(int * SP_NONNULL ptr);
+    #define SP_NONNULL _Nonnull
+    #define SP_NULL_UNSPECIFIED _Null_unspecified
+    // int fetch_or_zero(int * SP_NULLABLE ptr);
+    #define SP_NULLABLE _Nullable
 #else
-#define SP_NONNULL
-#define SP_NULL_UNSPECIFIED
-#define SP_NULLABLE
+    #define SP_NONNULL
+    #define SP_NULL_UNSPECIFIED
+    #define SP_NULLABLE
 #endif // defined(__clang__)
 // ------------- NULLABLE ------------------
 
@@ -80,50 +80,50 @@
 // .../usr/include/objc/NSObjCRuntime.h
 #if !__has_include(<NSObjCRuntime.h>)
 
-#if __LP64__ || 0 || NS_BUILD_32_LIKE_64
-typedef long NSInteger;
-typedef unsigned long NSUInteger;
-#else
-typedef int NSInteger;
-typedef unsigned int NSUInteger;
-#endif
+    #if __LP64__ || 0 || NS_BUILD_32_LIKE_64
+        typedef long NSInteger;
+        typedef unsigned long NSUInteger;
+    #else
+        typedef int NSInteger;
+        typedef unsigned int NSUInteger;
+    #endif
 
 #endif // !__has_include(<NSObjCRuntime.h>)
 // ------------- NSInteger ------------------
 
 // ------------- NSEnum ------------------
 #if defined(CF_ENUM)
-#define SP_ENUM CF_ENUM
-#define SP_OPTIONS CF_OPTIONS
+    #define SP_ENUM CF_ENUM
+    #define SP_OPTIONS CF_OPTIONS
 #else
-// .../CoreFoundation.framework/Headers/CFAvailability.h
-// Enums and Options
-#if __has_attribute(enum_extensibility)
-#define __SP_ENUM_ATTRIBUTES __attribute__((enum_extensibility(open)))
-#define __SP_CLOSED_ENUM_ATTRIBUTES __attribute__((enum_extensibility(closed)))
-#define __SP_OPTIONS_ATTRIBUTES __attribute__((flag_enum,enum_extensibility(open)))
-#else
-#define __SP_ENUM_ATTRIBUTES
-#define __SP_CLOSED_ENUM_ATTRIBUTES
-#define __SP_OPTIONS_ATTRIBUTES
+    // .../CoreFoundation.framework/Headers/CFAvailability.h
+    // Enums and Options
+    #if __has_attribute(enum_extensibility)
+        #define __SP_ENUM_ATTRIBUTES __attribute__((enum_extensibility(open)))
+        #define __SP_CLOSED_ENUM_ATTRIBUTES __attribute__((enum_extensibility(closed)))
+        #define __SP_OPTIONS_ATTRIBUTES __attribute__((flag_enum,enum_extensibility(open)))
+    #else
+        #define __SP_ENUM_ATTRIBUTES
+        #define __SP_CLOSED_ENUM_ATTRIBUTES
+        #define __SP_OPTIONS_ATTRIBUTES
 #endif
 
 #define __SP_ENUM_GET_MACRO(_1, _2, NAME, ...) NAME
 #if (__cplusplus && __cplusplus >= 201103L && (__has_extension(cxx_strong_enums) || \
     __has_feature(objc_fixed_enum))) || (!__cplusplus && __has_feature(objc_fixed_enum))
-#define __SP_NAMED_ENUM(_type, _name)     enum __SP_ENUM_ATTRIBUTES _name : _type _name; enum _name : _type
-#define __SP_ANON_ENUM(_type)             enum __SP_ENUM_ATTRIBUTES : _type
-#define SP_CLOSED_ENUM(_type, _name)      enum __SP_CLOSED_ENUM_ATTRIBUTES _name : _type _name; enum _name : _type
-#if (__cplusplus)
-#define SP_OPTIONS(_type, _name) _type _name; enum __SP_OPTIONS_ATTRIBUTES : _type
+    #define __SP_NAMED_ENUM(_type, _name)     enum __SP_ENUM_ATTRIBUTES _name : _type _name; enum _name : _type
+    #define __SP_ANON_ENUM(_type)             enum __SP_ENUM_ATTRIBUTES : _type
+    #define SP_CLOSED_ENUM(_type, _name)      enum __SP_CLOSED_ENUM_ATTRIBUTES _name : _type _name; enum _name : _type
+    #if (__cplusplus)
+        #define SP_OPTIONS(_type, _name) _type _name; enum __SP_OPTIONS_ATTRIBUTES : _type
+    #else
+        #define SP_OPTIONS(_type, _name) enum __SP_OPTIONS_ATTRIBUTES _name : _type _name; enum _name : _type
+    #endif
 #else
-#define SP_OPTIONS(_type, _name) enum __SP_OPTIONS_ATTRIBUTES _name : _type _name; enum _name : _type
-#endif
-#else
-#define __SP_NAMED_ENUM(_type, _name) _type _name; enum
-#define __SP_ANON_ENUM(_type) enum
-#define SP_CLOSED_ENUM(_type, _name) _type _name; enum
-#define SP_OPTIONS(_type, _name) _type _name; enum
+    #define __SP_NAMED_ENUM(_type, _name) _type _name; enum
+    #define __SP_ANON_ENUM(_type) enum
+    #define SP_CLOSED_ENUM(_type, _name) _type _name; enum
+    #define SP_OPTIONS(_type, _name) _type _name; enum
 #endif
 
 /* SP_ENUM supports the use of one or two arguments.
@@ -148,36 +148,32 @@ SP_ENUM(CFIndex) {
 
 // ------------- NS_SWIFT_NAME ------------------
 #if defined(CF_SWIFT_NAME)
-#define SP_SWIFT_NAME CF_SWIFT_NAME
+    #define SP_SWIFT_NAME CF_SWIFT_NAME
 #else
-// CoreFoundation.framework/Headers/CFBase.h
-#if __has_attribute(swift_name)
-# define SP_SWIFT_NAME(_name) __attribute__((swift_name(#_name)))
-#else
-# define SP_SWIFT_NAME(_name)
+    // CoreFoundation.framework/Headers/CFBase.h
+    #if __has_attribute(swift_name)
+        #define SP_SWIFT_NAME(_name) __attribute__((swift_name(#_name)))
+    #else
+        #define SP_SWIFT_NAME(_name)
+    #endif
 #endif
-#endif // defined(CF_ENUM)
 // ------------- NS_SWIFT_NAME ------------------
 
 // ------------- NS_NOESCAPE ------------------
 #ifdef NS_NOESCAPE
-
-#define SP_NOESCAPE NS_NOESCAPE
-
+    #define SP_NOESCAPE NS_NOESCAPE
 #else
-
-#if __has_attribute(noescape)
-#define SP_NOESCAPE __attribute__((noescape))
-#else
-#define SP_NOESCAPE
-#endif
-
+    #if __has_attribute(noescape)
+        #define SP_NOESCAPE __attribute__((noescape))
+    #else
+        #define SP_NOESCAPE
+    #endif
 #endif
 // ------------- NS_NOESCAPE ------------------
 
 // ------------- CAST ------------------
 #ifndef SP_OPAQUE_POINTER
-#define SP_OPAQUE_POINTER(x) typedef struct x##_t* x##_p
+    #define SP_OPAQUE_POINTER(x) typedef struct x##_t* x##_p
 #endif // SP_OPAQUE_POINTER
 
 #if (__cplusplus)
